892d2c8b30d0929e0928fd7d5093a77a9aceb8cd
Yves-ANAGUE <anagueyvessannong@gmail.com>
2026-07-13 03:02:25 +0100

    Version 2.2 du CMS HI CONSULTING IMMIGRATION

--- routes/televersementRoutes.js
+++ routes/televersementRoutes.js
@@ -8,7 +8,7 @@ routeur.post(
   '/',
   verifierAuthentification,
   autoriserRoles('admin', 'super_admin', 'collaborateur'),
-  televersementMultiple.array('fichiers', 15),
+  televersementMultiple.array('fichiers', 20),
   televersementController.televerserMedias
 );
 
