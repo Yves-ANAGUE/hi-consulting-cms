ae5f0c511eebd099c0fc9d147e040d730d2a97ce
Yves-ANAGUE <anagueyvessannong@gmail.com>
2026-08-04 11:53:47 +0100

    optimisation accueil 2

--- controllers/pagesPubliquesController.js
+++ controllers/pagesPubliquesController.js
@@ -73,7 +73,7 @@ async function obtenirParametresSite() {
     urlLogo: ligne?.url_logo || '/static/images/logo_HICI.jpg',
     whatsappFlottant: ligne?.url_whatsapp_flottant || '',
     urlImageMission: ligne?.url_image_mission || 'https://res.cloudinary.com/jmqph5iy/image/upload/v1785835978/photo_mission_le5ce8.jpg',
-    urlImageVision: ligne?.url_image_vision || 'https://res.cloudinary.com/jmqph5iy/image/upload/v1785838881/image_notre_vision_q91sli.jpg',
+    urlImageVision: ligne?.url_image_vision || 'https://res.cloudinary.com/jmqph5iy/image/upload/v1785840405/image_vision_biyvu1.png',
     blocCarteGoogleMaps: urlGoogleMaps
       ? `<iframe src="${urlGoogleMaps}" width="100%" height="300" style="border:0;border-radius:10px;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
       : `<p class="texte-secondaire">Localisation Google Maps configurable depuis le Back-Office (onglet Configuration Google).</p>`,
--- scripts/initialiserBaseDeDonnees.js
+++ scripts/initialiserBaseDeDonnees.js
@@ -452,7 +452,7 @@ async function seederConfigurationSite() {
       '237678924045',
       'https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d761.5990300722099!2d9.767887998886145!3d4.080652348274601!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sLogpom%2C%20Carrefour%20Bassong%2C%20Douala!5e0!3m2!1sfr!2scm!4v1783452413876!5m2!1sfr!2scm',
       'https://res.cloudinary.com/jmqph5iy/image/upload/v1785835978/photo_mission_le5ce8.jpg',
-      'https://res.cloudinary.com/jmqph5iy/image/upload/v1785838881/image_notre_vision_q91sli.jpg'
+      'https://res.cloudinary.com/jmqph5iy/image/upload/v1785840405/image_vision_biyvu1.png'
     ]
   );
   console.log('[Seed] Numéro WhatsApp flottant, lien Google Maps et images Mission/Vision initialisés par défaut.');
